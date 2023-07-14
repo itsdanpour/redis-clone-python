7a4fe96ea6fb2d1911562ad6151f8ff3cd7dc62e
Mohammad Mohsen-Pour <mohammad.mohsenpour1@gmail.com>
2023-07-14 16:28:40 -0400

    respond with PONG for each request until connection/socket is closed by cliet

--- app/main.py
+++ app/main.py
@@ -3,15 +3,16 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.listen()
     conn, add = server_socket.accept()  # wait for client
     with server_socket:
-        while True:
-            with conn:
+        with conn:
+            while True:
                 data = conn.recv(1024)
                 if not data:
                     break
                 conn.send(b"+PONG\r\n")
+
+
 
 
 if __name__ == "__main__":
